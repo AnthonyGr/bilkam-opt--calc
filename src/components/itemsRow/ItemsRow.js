989985d7704228f80fd952ad660ce462816a706b
Anton Grishin <grishin.ant.a@gmail.com>
2022-03-19 23:03:48 +0500

    Автоматическое формирование всех селектов и подсчет суммы

--- src/components/itemsRow/ItemsRow.js
+++ src/components/itemsRow/ItemsRow.js
@@ -1,15 +1,32 @@
-const ItemsRow = ({ name, data }) => {
+import { useState } from "react";
+
+const ItemsRow = ({ name, type, data, onCostChange }) => {
+  const [selectedItem, setSelectedItem] = useState(0);
+
   const fillSelect = () => {
-    const items = data.map((item, i) => <option key={i}>{item.name}</option>);
+    const items = data.map((item, i) => (
+      <option key={i} value={item.price}>
+        {item.name}
+      </option>
+    ));
+
     return (
       <select
         className="form-select mb-10"
         aria-label="Default select example"
-        id="stella"
+        data-type={type}
+        onChange={onSelectChange}
+        value={selectedItem}
       >
+        <option value="0">Не выбрано</option>
         {items}
       </select>
     );
+  };
+
+  const onSelectChange = (e) => {
+    setSelectedItem(e.target.value);
+    onCostChange(e.target.getAttribute("data-type"), e.target.value);
   };
 
   const element = fillSelect();
@@ -17,7 +34,7 @@ const ItemsRow = ({ name, data }) => {
     <div className="row align-items-start">
       <div className="col">{name}</div>
       <div className="col">{element}</div>
-      <div className="col">10 000</div>
+      <div className="col">{selectedItem}</div>
     </div>
   );
 };
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
 import ItemsRow from "../itemsRow/ItemsRow";
 
 import steles from "../../data/steles.json";
+import pedestals from "../../data/pedestals.json";
+import borders from "../../data/borders.json";
 
 import "./main.css";
 
 function Main() {
+  const [order, setOrder] = useState({});
+  const [totalCost, setTotalCost] = useState(0);
+
+  const onCostChange = (type, cost) => {
+    setOrder((prevState) => ({
+      ...prevState,
+      [type]: cost,
+    }));
+  };
+
+  const calcOrderCost = () => {
+    let cost = 0;
+    for (const [key, value] of Object.entries(order)) {
+      cost += +value;
+    }
+    setTotalCost(cost);
+  };
+
+  useEffect(() => {
+    calcOrderCost();
+  });
+
   return (
     <div className="main">
       <h1 className="">
@@ -14,12 +39,56 @@ function Main() {
       </h1>
 
       <div className="container">
-        <ItemsRow name={"Стела: "} data={steles}></ItemsRow>
-        <ItemsRow name={"Тумба: "} data={steles}></ItemsRow>
-        <ItemsRow name={"Цветник 1: "} data={steles}></ItemsRow>
-        <ItemsRow name={"Цветник 2: "} data={steles}></ItemsRow>
-        <ItemsRow name={"Цветник 3: "} data={steles}></ItemsRow>
-        <ItemsRow name={"Цветник 4: "} data={steles}></ItemsRow>
+        <ItemsRow
+          name={"Стела: "}
+          type={"stele"}
+          data={steles}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+        <ItemsRow
+          name={"Изготовление формы: "}
+          type={"form"}
+          data={steles}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+        <ItemsRow
+          name={"Тумба: "}
+          type={"pedestal"}
+          data={pedestals}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+        <ItemsRow
+          name={"Цветник 1: "}
+          type={"border-1"}
+          data={borders}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+        <ItemsRow
+          name={"Цветник 2: "}
+          type={"border-2"}
+          data={borders}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+        <ItemsRow
+          name={"Цветник 3: "}
+          type={"border-3"}
+          data={borders}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+        <ItemsRow
+          name={"Цветник 4: "}
+          type={"border-4"}
+          data={borders}
+          onCostChange={onCostChange}
+        ></ItemsRow>
+
+        <div className="row align-items-start">
+          <div className="col">Общая стоимость:</div>
+          <div className="col">{totalCost}</div>
+          <div className="col">
+            <button>Заказать</button>
+          </div>
+        </div>
       </div>
     </div>
   );
